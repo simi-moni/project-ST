f01cbf3615e1e78a1fc252165a8edcb31d208f25
GRaykovski <59851138+GRaykovski@users.noreply.github.com>
2020-12-20 19:31:43 +0200

    Add files via upload

---
+++ dbo/Tables/Card.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Card]
+(
+	[CardId] INT NOT NULL PRIMARY KEY, 
+    [Type] NVARCHAR(50) NOT NULL, 
+    [Duration] DATETIME NOT NULL, 
+    [Price] MONEY NOT NULL, 
+    [UserId] INT NOT NULL, 
+    [CreatedOn] DATE NOT NULL, 
+    [Modified] DATE NOT NULL, 
+    [isValid] UNIQUEIDENTIFIER NOT NULL, 
+    [QRcore] INT NOT NULL, 
+    CONSTRAINT [FK_Card_UserId] FOREIGN KEY ([UserId]) REFERENCES [User]([UserId])
+)
---
+++ dbo/Tables/User.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[UserId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProfilePicture] IMAGE NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Email] NVARCHAR(MAX) NOT NULL, 
+    [CreatedOn] DATE NOT NULL, 
+    [DeletedOn] DATE NOT NULL, 
+    [Modified] DATE NOT NULL, 
+    [Password] NVARCHAR(MAX) NOT NULL, 
+    [Acc_Type] NCHAR(10) NOT NULL, 
+    [User_Type] NCHAR(10) NOT NULL, 
+    [CreditCard] BIGINT NOT NULL
+)
