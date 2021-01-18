b0d4dee05d2a6236f4c950f29549fd6ef415a331
Andrea Denikova <andreadenikovich@gmail.com>
2021-01-18 18:28:53 +0200

    project created

---
+++ OnlineTickets/OnlineTickets.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineTickets.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "OnlineTickets";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
---
+++ OnlineTickets/Web/OnlineTickets.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,14 @@
+﻿namespace OnlineTickets.Web.Areas.Administration.Controllers
+{
+    using OnlineTickets.Common;
+    using OnlineTickets.Web.Controllers;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    [Area("Administration")]
+    public class AdministrationController : BaseController
+    {
+    }
+}
