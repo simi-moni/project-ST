a8bdd0af4f84685f093600326a8307cb01358e02
SpasPandev <74670884+SpasPandev@users.noreply.github.com>
2021-01-18 16:01:19 +0200

    Add files via upload

---
+++ layout/Forgot_Password.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project.Resources.layout
+{
+    [Activity(Label = "Forgot_Password")]
+    public class Forgot_Password : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.Forgot_Password);
+
+            Button button_Reset_Password = FindViewById<Button>(Resource.Id.btn_resset_password);
+
+            button_Reset_Password.Click += delegate { StartActivity(typeof(LogIn)); };
+
+            Button button_Registration = FindViewById<Button>(Resource.Id.btn_register_here);
+
+            button_Registration.Click += delegate { StartActivity(typeof(Registration)); };
+
+            Button button_to_Login = FindViewById<Button>(Resource.Id.btn_back);
+
+            button_to_Login.Click += delegate { StartActivity(typeof(LogIn)); };
+        }
+    }
+}
---
+++ layout/LogIn.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project.Resources.layout
+{
+    [Activity(Label = "LogIn")]
+    public class LogIn : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.LogIn);
+
+            Button button_Forgot_Password = FindViewById<Button>(Resource.Id.btn_forgot_password);
+
+            button_Forgot_Password.Click += delegate { StartActivity(typeof(Forgot_Password)); };
+
+            Button button_LogIn = FindViewById<Button>(Resource.Id.btn_login);
+
+            button_LogIn.Click += delegate { StartActivity(typeof(Home)); };
+
+            Button button_Registration = FindViewById<Button>(Resource.Id.btn_register_here);
+
+            button_Registration.Click += delegate { StartActivity(typeof(Registration)); };
+        }
+    }
+}
